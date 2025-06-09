22a257d8b264f5f1de78001ffbb9699f6030c37e
Ethan Haggart <haggarter@gmail.com>
2025-06-09 12:22:00 -0600

    usage refactored

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include "smartssd.h"
 
+static char *usage = "Usage: ./query_smart [string: drive name] [int: number of cycles]\n";
+
 int main(int argc, char *argv[]) {
-    
+
     if (argc != 3) {
-        printf("Too few arguments.\nUsage: ./query_smart [string: drive name] [int: number of cycles]\n");
+        printf("Too few arguments.\n%s\n", usage);
         exit(1);
     }
 
     int cycles;
 
     if ((cycles = atoi(argv[2])) < 1) {
-        printf("Cycles must be an integer greater than 0.\n");
+        printf("Cycles must be an integer greater than 0.\n%s\n", usage);
         exit(1);
     }
     
