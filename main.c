9e7e4f8c353ad7ee99d19f52bf7d0965665e20e7
Ethan Haggart <haggarter@gmail.com>
2025-06-09 12:18:42 -0600

    cycles check

--- main.c
+++ main.c
@@ -6,6 +6,13 @@ int main(int argv, char *argc[]) {
         printf("Too few arguments.\nUsage: ./query_smart [string: drive name] [int: number of cycles]\n");
         exit(1);
     }
+
+    int cycles;
+
+    if ((cycles = atoi(argv[2])) < 1) {
+        printf("Cycles must be an integer greater than 0.\n");
+        exit(1);
+    }
     
     smartssd dev;
     return 0;
