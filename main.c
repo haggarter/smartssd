304960b1ca2418073a3c17780ce98f07047d61c2
Ethan Haggart <haggarter@gmail.com>
2025-06-09 12:15:14 -0600

    main stub fixed

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argv, char *argc[]) {
     
     if (argv != 3) {
         printf("Too few arguments.\nUsage: ./query_smart [string: drive name] [int: number of cycles]\n");
-        exit();
+        exit(1);
     }
     
     smartssd dev;
