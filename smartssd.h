b76f3d110e57cf5c2b24e0dce2f237128d73da49
Ethan Haggart <haggarter@gmail.com>
2025-06-09 11:51:43 -0600

    init

---
+++ smartssd.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+#include <atasmart.h>
+#include <nvme/nvme.h>
+
+#define NVME 0
+#define SATA 1
+#define UNKNOWN -1
+
+typedef struct smartssd {
+    char type;
+    const char *path;
+    struct nvme_dev *nvme_drive;
+    SkDisk *sata_drive;
+} smartssd;
+
+int smartssd_init() {
+
+}
+
+int smartssd_deinit() {
+
+}
+
